0e9d4ce8b63e9c2b37536ffd5d49025e1e529809
lavondedunigan <93886681+lavondedunigan@users.noreply.github.com>
2024-12-17 12:28:44 -0600

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,34 @@
+console.log("---------------Story---------------");
+// 1. Declare Variables
+// Using let
+
+let character = "Kenya";
+let action = "cheer";
+let place = "Arrowhead";
+let object = "wolf";
+let time = "beautiful morning";
+let anotherTime = "gloomy evening";
+
+// Using "Const"
+const familyMember = "husband";
+
+// 2. Create the Story
+console.log("One day " + person + "was late for " + place + " It was" + time);
+console.log("What time do the Chiefs play " + anotherTime +".");
+console.log("My " + familyMember + "was there on time");
+
+// 3. Experient with Different Values
+console.log("--------------- Another Story---------------");
+
+// Change values to create a different story
+let character = "Diane";
+let action = "paint";
+let place = "Artshow";
+let time = "sunny afternoon";
+let anotherTime = "bright morning";
+
+console.log("One day " + name + "was late for " + place + " It was" + time);
+console.log("What time do the Chiefs play " + anotherTime +".");
+console.log("My " + familyMember + "was there on time");
+
+// Note "const" variables cannot be reassigned, so keep that in mind when experimenting.
